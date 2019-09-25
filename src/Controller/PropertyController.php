0bea3538e41a72e6b6dd76b4e29a83fa21469ec9
Chris <cj.carlier@gmail.com>
2019-09-25 21:58:22 +0200

    Pagination et filtre

--- src/Controller/PropertyController.php
+++ src/Controller/PropertyController.php
@@ -2,8 +2,12 @@
 namespace App\Controller;
 
 use App\Entity\Property;
+use App\Entity\PropertySearch;
+use App\Form\PropertySearchType;
 use App\Repository\PropertyRepository;
 use Doctrine\Common\Persistence\ObjectManager;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -28,18 +32,32 @@ class PropertyController extends AbstractController
 
     /**
      * @Route("/biens", name="property.index")
+     * @param PaginatorInterface $paginator
+     * @param Request $request
      * @return Response
      */
-    public function index(): Response
+    public function index(PaginatorInterface $paginator, Request $request): Response
     {
-        
+        $search = new PropertySearch();
+        $form = $this->createForm(PropertySearchType::class, $search);
+        $form->handleRequest($request);
+
+        $properties = $paginator->paginate(
+            $this->repository->findAllVisibleQuery($search),
+            $request->query->getInt('page', 1),
+            12
+        );
         return $this->render('property/index.html.twig',[
-            'current_menu' => 'properties' // paramètre envoyé à la partie template
+            'current_menu'  => 'properties',
+            'properties'    => $properties,
+            'form'          => $form->createView()
         ]);
     }
 
     /**
      * @Route("/biens/{slug}-{id}", name="property.show", requirements={"slug":"[a-z0-9\-]*"})
+     * @param Property $property
+     * @param string $slug
      * @return Response
      */
     // public function show($slug, $id): Response
